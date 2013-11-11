5025408189f53a431f24c455304e39bd097ef0a9
miori <enomotomi@systena.co.jp>
2013-11-12 01:21:00 +0900

    2013/11/11

--- booksearch.js
+++ booksearch.js
@@ -1,11 +1,13 @@
-jQuery(function($) {
+var count = 0;
+var page_count = 1;
+var html = "";
 
-	var page_count = 1;
-	var html = "";
+jQuery(function($) {
 	
     $('#submit').click(function() {
  
 		$("#search_result").html("");
+
         // ポストデータ
         var data = {};
 		if ($('input[name="title"]').val()!="") {
@@ -14,11 +16,12 @@ jQuery(function($) {
 		if ($('input[name="author"]').val()!="") {
 			data.author = $('input[name="author"]').val();
 		};
+		page_count = 1;
 		data.hits = "20";
 		data.page = page_count;
         // Ajax通信
         $.ajax({
-            url: 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?format=json&booksGenreId=001&applicationId=1035572035589402260&jsoncallback=result',
+            url: 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?format=json&booksGenreId=001&applicationId=E&jsoncallback=result',
             type: 'POST',
             data: data,
             dataType: 'jsonp',
@@ -26,7 +29,7 @@ jQuery(function($) {
 				var items = response.Items
 				html = "";
 				for (var i = 0, j = items.length; i < j; i++) {
-					if(i == 0 || i % 3 == 0){
+					if(count % 3 == 0){
 						html += '<div class="boxContainer">';
 					}
 					html += '<div class="box"><img src=\"' + items[i].Item.largeImageUrl + '\"><br>';
@@ -34,7 +37,8 @@ jQuery(function($) {
 					html += '<tr><td>著者名</td><td>' + items[i].Item.author + "</td></tr></table>";
 					html += "<Form><input name=\"openWin\" type=\"button\" value=\"詳細表示\" onClick=\"window.open('http://tobenai233.web.fc2.com/detail.html?" + items[i].Item.isbn + "','','scrollbars=yes,width=600,height=600,');\"></Form>"
 					html += '</div>';
-					if(i - 2 == 0 || (i - 2) % 3 == 0 || i == j){
+					count += 1;
+					if(count % 3 == 0){
 						html += '</div>';
 					}
 					$("#search_result").html(html);
@@ -62,14 +66,14 @@ jQuery(function($) {
 		data.page = page_count;
         // Ajax通信
         $.ajax({
-            url: 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?format=json&booksGenreId=001&applicationId=1035572035589402260&jsoncallback=result',
+            url: 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?format=json&booksGenreId=001&applicationId=&jsoncallback=result',
             type: 'POST',
             data: data,
             dataType: 'jsonp',
             success: function(response) {
 				var items = response.Items
 				for (var i = 0, j = items.length; i < j; i++) {
-					if(i == 0 || i % 3 == 0){
+					if(count % 3 == 0){
 						html += '<div class="boxContainer">';
 					}
 					html += '<div class="box"><img src=\"' + items[i].Item.largeImageUrl + '\"><br>';
@@ -77,7 +81,8 @@ jQuery(function($) {
 					html += '<tr><td>著者名</td><td>' + items[i].Item.author + "</td></tr></table>";
 					html += "<Form><input name=\"openWin\" type=\"button\" value=\"詳細表示\" onClick=\"window.open('http://tobenai233.web.fc2.com/detail.html?" + items[i].Item.isbn + "','','scrollbars=yes,width=600,height=600,');\"></Form>"
 					html += '</div>';
-					if(i - 2 == 0 || (i - 2) % 3 == 0 || i == j){
+					count += 1;
+					if(count % 3 == 0){
 						html += '</div>';
 					}
 					$("#search_result").html(html);
@@ -87,16 +92,35 @@ jQuery(function($) {
         });
         return false;
     });
+
+    //スクロール時にTOPへもどるボタン表示
+	$(window).scroll(function () {
+	    if ($(this).scrollTop() > 100) {
+	        $("#back").fadeIn();
+	    } else {
+	        $("#back").stop().fadeOut();
+	    }
+	});
+	// scroll body to 0px on click
+	$("#back a").click(function () {
+	    $("body,html").animate({
+	        scrollTop: 0
+	    }, 800);
+	    return false;
+	});
+
 });
 
+    
 $(window).load(function() {
 	$("#more").hide();
-	$.getJSON("https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?format=json&booksGenreId=001&applicationId=1035572035589402260&jsoncallback=result",
+
+	$.getJSON("https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?format=json&booksGenreId=001&applicationId=&jsoncallback=result",
 		function(result){
 			var items = result.Items
 			var html = "";
 			for (var i = 0, j = items.length; i < j; i++) {
-				if(i == 0 || i % 3 == 0){
+				if(count % 3 == 0){
 					html += '<div class="boxContainer">';
 				}
 				html += '<div class="box"><img src=\"' + items[i].Item.largeImageUrl + '\"><br>';
@@ -104,7 +128,8 @@ $(window).load(function() {
 				html += '<tr><td>著者名</td><td>' + items[i].Item.author + "</td></tr></table>";
 				html += "<Form><input name=\"openWin\" type=\"button\" value=\"詳細表示\" onClick=\"window.open('http://tobenai233.web.fc2.com/detail.html?" + items[i].Item.isbn + "','','scrollbars=yes,width=600,height=600,');\"></Form>"
 				html += '</div>';
-				if(i - 2 == 0 || (i - 2) % 3 == 0 || i == j){
+				count += 1;
+				if(count % 3 == 0){
 					html += '</div>';
 				}
 				$("#search_result").html(html);
